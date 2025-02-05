633529a19fda2d19a9083766a2241d0fedc793c8
byoung joo chae <wpdhkd1642@gmail.com>
2025-02-05 23:49:28 +0900

    first commit

--- cag.py
+++ cag.py
@@ -11,7 +11,7 @@ import json
 import pandas as pd
 import os
 
-os.environ["LANGSMITH_API_KEY"] = "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"
+os.environ["LANGSMITH_API_KEY"] = ""
 os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
 os.environ["LANGSMITH_PROJECT"]  = "CRAG"
 os.environ["LANGSMITH_TRACING"] = "true"
