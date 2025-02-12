d450f70ed698ebf6887ca2d7f5b0538ff63285b0
byungjuchae <63639887+byoungjuchae@users.noreply.github.com>
2025-02-12 18:25:04 +0900

    Update multiple_rag.py

--- multiple_rag.py
+++ multiple_rag.py
@@ -21,34 +21,47 @@ def get_unique_union(documents: list[list]):
     flattened_docs = [dumps(doc) for sublist in documents for doc in sublist]
     
     unique_docs = list(set(flattened_docs))
-    
+
     return [loads(doc) for doc in unique_docs]
+
+def sentences(documents:str):
+
+    docs = []
+    doc_splits = documents.split('\n')
+    for doc in doc_splits:
+
+        if doc == '':
+            pass
+        else:
+            docs.append(doc)
+    return docs
 def multiple_query(question,vectorstore):
     
-    prompt_text = """ You are an language assistant. You create the five sentences similar to the {question}.And
-    answer contains the \n at the last. and only respond the five senteces answer.
+    prompt_text = """ You are an AI research assistant. You create the ten sentences similar to the {question}.and only respond the ten sentences answer.
+    not include the Here are ten possible anwers.
     Here is the question:
     {question}
     
     """
     prompt = ChatPromptTemplate.from_template(prompt_text)
-    llm = ChatOllama(model='llama3.2:latest')
-    multiple_chain = (prompt | llm | StrOutputParser()|(lambda x:x.split('\n')))
-  
+    llm = ChatOllama(model='llama3.2:latest',temperature=0)
+
+    multiple_chain = (prompt | llm | StrOutputParser()| sentences)
+    
     multiple_query = multiple_chain | vectorstore.map() | get_unique_union
     
     return multiple_query
 
 if __name__ == "__main__":
     
-    pdf_path = './files/'
+    pdf_path = './files'
     docs = PyPDFDirectoryLoader(pdf_path).load()
-    text_split = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=250)
+    text_split = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=500,chunk_overlap=250)
     doc = text_split.split_documents(docs)
     embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2')
     retriever = FAISS.from_documents(doc,embedding=embedding).as_retriever()
-    llm = ChatOllama(model='llama3.2:latest')
-    question = "What is SOP?"
+    llm = ChatOllama(model='llama3.2:latest',temperature=0)
+    question = "What is Reward-Guided Speculative Decoding Algorithm?"
     prompt_text = """ You are an AI assistant. you must answer the {question}. you retrieve the {context}.
     
     Here is the question:
