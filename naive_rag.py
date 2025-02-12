e16fbac6d034f78f698ab53032f95cb0018f3c43
byungjuchae <63639887+byoungjuchae@users.noreply.github.com>
2025-02-12 18:24:32 +0900

    Update naive_rag.py

--- naive_rag.py
+++ naive_rag.py
@@ -1,4 +1,4 @@
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_ollama.chat_models import ChatOllama
@@ -12,12 +12,13 @@ import os
 os.environ['LANGSMITH_API_KEY'] = ""
 os.environ['LANGSMITH_TRACING'] = "true"
 os.environ["LANGSMITH_PROJECT"] = "CRAG"
+os.environ['LANGSMITH_ENDPOINT'] = "https://api.smith.langchain.com"
 
 
 if __name__ == '__main__':
-    pdf_file = './2309.07870v3.pdf'
-    docs = PyPDFLoader(pdf_file).load()
-    text_split = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=250)
+    pdf_file = './files'
+    docs = PyPDFDirectoryLoader(pdf_file).load()
+    text_split = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=500,chunk_overlap=250)
     docs = text_split.split_documents(docs)
     embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2')
     vectorstore = FAISS.from_documents(docs,embedding=embedding).as_retriever()
@@ -31,6 +32,6 @@ if __name__ == '__main__':
     prompt = ChatPromptTemplate.from_template(prompt_text)
     chain = ({"context":vectorstore,"question":RunnablePassthrough()}| prompt | llm | StrOutputParser())
     
-    response = chain.invoke("What is SOP?")
+    response = chain.invoke("What is Reward-Guided Speculative Decoding Algorithm?",temperature=0)
 
     print(response)
